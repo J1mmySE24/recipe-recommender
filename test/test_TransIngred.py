92daed7c64638415a24209bd8e7c810ba85fe1a7
shreyasdvrj <shreyas.dev22@gmail.com>
2024-11-21 15:57:12 -0500

    make repo stable + change env to ours

--- test/test_TransIngred.py
+++ test/test_TransIngred.py
@@ -1,5 +1,5 @@
 import requests
 
 def test() :
-    result = requests.get("http://localhost:5000/api/v1/recipes?Cleaned-Ingredients=tomato&Cuisine=Mexican").json()
+    result = requests.get("http://localhost:5001/api/v1/recipes?Cleaned-Ingredients=tomato&Cuisine=Mexican").json()
     assert len(result['recipes'][0]['TranslatedInstructions']) != 0
